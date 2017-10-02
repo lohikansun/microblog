deb9a2e3faa95827d158accf186338acf6711bf3
Dylan Pincus <lohikansun@gmail.com>
2017-10-02 01:50:40 -0400

    unfollowing working

--- lib/microblog_web/controllers/follow_controller.ex
+++ lib/microblog_web/controllers/follow_controller.ex
@@ -4,16 +4,11 @@ defmodule MicroblogWeb.FollowController do
   alias Microblog.Accounts
   alias Microblog.Accounts.Follow
 
-  def index(conn, _params) do
-    follows = Accounts.list_follows()
-    render(conn, "index.html", follows: follows)
-  end
-  
   def follow(conn, params) do
     case Accounts.create_follow(params) do
       {:ok, message} ->
         conn
-        |> put_flash(:info, "Message created successfully.")
+        |> put_flash(:info, "Followed.")
         |> redirect(to: user_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
@@ -23,47 +18,12 @@ defmodule MicroblogWeb.FollowController do
 
   end
   
-  def new(conn, _params) do
-    changeset = Accounts.change_follow(%Follow{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"follow" => follow_params}) do
-    case Accounts.create_follow(follow_params) do
-      {:ok, follow} ->
-        conn
-        |> put_flash(:info, "Follow created successfully.")
-        |> redirect(to: follow_path(conn, :show, follow))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  def edit(conn, %{"id" => id}) do
-    follow = Accounts.get_follow!(id)
-    changeset = Accounts.change_follow(follow)
-    render(conn, "edit.html", follow: follow, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "follow" => follow_params}) do
-    follow = Accounts.get_follow!(id)
-
-    case Accounts.update_follow(follow, follow_params) do
-      {:ok, follow} ->
-        conn
-        |> put_flash(:info, "Follow updated successfully.")
-        |> redirect(to: follow_path(conn, :show, follow))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", follow: follow, changeset: changeset)
-    end
-  end
 
   def delete(conn, %{"id" => id}) do
     follow = Accounts.get_follow!(id)
     {:ok, _follow} = Accounts.delete_follow(follow)
-
     conn
-    |> put_flash(:info, "Follow deleted successfully.")
-    |> redirect(to: follow_path(conn, :index))
+    |> put_flash(:info, "Unfollowed.")
+    |> redirect(to: user_path(conn, :index))
   end
 end
--- lib/microblog_web/views/follow_view.ex
+++ lib/microblog_web/views/follow_view.ex
@@ -1,3 +1,10 @@
 defmodule MicroblogWeb.FollowView do
   use MicroblogWeb, :view
+
+  def get_follow(user, follow) do
+    u = user |> Microblog.Repo.preload(:follows)
+    Enum.find(u.follows, fn(x) -> (x.user_id == user.id) and (x.follow_id == follow) end)
+  end
+
+
 end
