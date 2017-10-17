50e6951f6f7d86d03494fdd4105d4f15fa3f347f
Dylan Pincus <lohikansun@gmail.com>
2017-10-17 13:25:41 -0400

    token

--- lib/microblog_web.ex
+++ lib/microblog_web.ex
@@ -33,7 +33,7 @@ defmodule MicroblogWeb do
                         namespace: MicroblogWeb
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
